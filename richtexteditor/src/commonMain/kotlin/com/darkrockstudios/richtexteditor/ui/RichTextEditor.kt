afee5e2079cde97e21b4e589640047150a5c87b5
Wavesonics <adamwbrown@gmail.com>
2023-02-02 20:21:08 -0800

    Expose read only

--- richtexteditor/src/commonMain/kotlin/com/darkrockstudios/richtexteditor/ui/RichTextEditor.kt
+++ richtexteditor/src/commonMain/kotlin/com/darkrockstudios/richtexteditor/ui/RichTextEditor.kt
@@ -9,7 +9,8 @@ fun RichTextEditor(
     value: RichTextValue,
     onValueChange: (RichTextValue) -> Unit,
     modifier: Modifier = Modifier,
-    textFieldStyle: RichTextFieldStyle = defaultRichTextFieldStyle()
+    textFieldStyle: RichTextFieldStyle = defaultRichTextFieldStyle(),
+    readOnly: Boolean = false,
 ) {
     RichTextField(
         modifier = modifier,
@@ -22,6 +23,7 @@ fun RichTextEditor(
                 onValueChange(newValue)
             }
         },
-        textFieldStyle = textFieldStyle
+        textFieldStyle = textFieldStyle,
+        readOnly = readOnly,
     )
 }
